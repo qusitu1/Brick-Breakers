ef3df42716752c23b290ab5dc21e3c0c0fef520f
aowen7 <aowen@s.sfusd.edu>
2023-11-28 15:49:05 -0800

    added game lost sound

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -6,11 +6,16 @@ class_name UI
 @onready var game_lost_container = $GameLostContainer
 @onready var level_won_container = $LevelWonContainer
 
+@onready var game_lost = $"../Sound/GameLost"
+
+
 func set_lifes(lifes: int):
 	lifes_label.text = "Lifes: %d" % lifes
 
 func game_over():
 	game_lost_container.show()
+	
+	game_lost.play()
 
 func _on_game_lost_button_pressed():
 	get_tree().reload_current_scene()
